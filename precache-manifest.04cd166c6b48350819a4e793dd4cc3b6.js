69ef4ae08809fb7cae14335cce45606443692f8a
Javier Silva <javitsilva@gmail.com>
2018-12-28 18:35:23 -0600

    Updates

---
+++ precache-manifest.04cd166c6b48350819a4e793dd4cc3b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a65393b01dab0d8438d",
+    "url": "/stocks-app/static/js/runtime~main.1a65393b.js"
+  },
+  {
+    "revision": "38395cb4a4d1ad851ecb",
+    "url": "/stocks-app/static/js/main.38395cb4.chunk.js"
+  },
+  {
+    "revision": "01e219221a74b39c5f47",
+    "url": "/stocks-app/static/js/1.01e21922.chunk.js"
+  },
+  {
+    "revision": "38395cb4a4d1ad851ecb",
+    "url": "/stocks-app/static/css/main.27fe673d.chunk.css"
+  },
+  {
+    "revision": "92d5afbc0fd9855345579a96430b3bd9",
+    "url": "/stocks-app/index.html"
+  }
+];
