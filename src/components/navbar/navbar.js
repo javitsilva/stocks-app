c24d7b25ee92b32a02f0cb21ef21f62d6b6da86a
Javier Silva <javitsilva@gmail.com>
2019-01-06 22:18:02 -0600

    dropdown mobile toggle, gotta fix wide one first

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import './navbar.css';
 import { NavLink } from 'react-router-dom';
+import DropdownMenu from './dropdown-menu';
 
 const Navbar = () => {
+  let dropdownMenu = (
+    <div className="dropdown-menu">
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><a href="/">Stocks</a></li>
+        <li><a href="/">Earnings</a></li>
+        <li><a href="/">Dividends</a></li>
+        <li><a href="/">Key Stats</a></li>
+        <li><a href="/">IPO Calendar</a></li>
+      </ul>
+      <ul>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>            
+    </div>
+  );
+
   return (
     <div>
       <nav id='wide-nav'>
@@ -10,23 +28,7 @@ const Navbar = () => {
           <NavLink to="/">$M</NavLink>
         </div>
         <input className="search-bar" type="text" placeholder="Search.."></input>
-        <div className="dropdown">
-          <div className="dropdown-arrow"></div>          
-          <div className="dropdown-menu">
-            <ul>
-              <li><NavLink to="/">Home</NavLink></li>
-              <li><a href="/">Stocks</a></li>
-              <li><a href="/">Earnings</a></li>
-              <li><a href="/">Dividends</a></li>
-              <li><a href="/">Key Stats</a></li>
-              <li><a href="/">IPO Calendar</a></li>
-            </ul>
-            <ul>
-              <li><NavLink to="/about">About</NavLink></li>
-              <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>            
-          </div>
-        </div>
+        <DropdownMenu mobile={false}/>
       </nav>    
       
       {/* mobile nav     */}
@@ -34,7 +36,7 @@ const Navbar = () => {
         <div className="logo">
           <NavLink to="/">$M</NavLink>
         </div>
-        <h1 style={{float: 'right', color: 'purple'}}>TODO</h1>
+        <DropdownMenu mobile={true} />
       </nav>  
 
       <div className='navbar-fixed-reserved-block'></div>  
